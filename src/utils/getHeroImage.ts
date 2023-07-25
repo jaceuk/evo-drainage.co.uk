6d654e162214927b2082fb907e90d2a656910a2d
jaceuk <jnewington@gmail.com>
2023-07-25 20:57:56 +0100

    Build

--- src/utils/getHeroImage.ts
+++ src/utils/getHeroImage.ts
@@ -1,22 +1,16 @@
-import PlumbingImage from '../images/bg-plumbing.jpg';
-import BlockedSinksToiletsImage from '../images/bg-blocked-sinks-toilets.jpg';
-import CentralHeatingImage from '../images/bg-central-heating.jpg';
-import BoilerServicingRepairsImage from '../images/bg-boiler-servicing-repairs.jpg';
-import PowerFlushingImage from '../images/bg-power-flushing.jpg';
-import GutterCleaningImage from '../images/bg-gutter-cleaning.jpg';
-import PatioCleaningImage from '../images/bg-patio-cleaning.jpg';
-import RoofCleaningImage from '../images/bg-roof-cleaning.jpg';
-import HomeImage from '../images/bg-home.jpg';
+import BlockedDrains from '../images/bg-blocked-drains.jpg';
+import CctvDrainSurveys from '../images/bg-cctv-drain-surveys.jpg';
+import DrainRepairs from '../images/bg-drain-repairs.jpg';
+import Soakaways from '../images/bg-soakaways.jpg';
+import GutterCleaning from '../images/bg-gutter-cleaning.jpg';
+import Home from '../images/bg-home.jpg';
 
 export function getHeroImage(serviceSlug: string): ImageMetadata {
-	if (serviceSlug === 'plumbing') return PlumbingImage;
-	if (serviceSlug === 'blocked-sinks-toilets') return BlockedSinksToiletsImage;
-	if (serviceSlug === 'central-heating') return CentralHeatingImage;
-	if (serviceSlug === 'boiler-servicing-repairs') return BoilerServicingRepairsImage;
-	if (serviceSlug === 'power-flushing') return PowerFlushingImage;
-	if (serviceSlug === 'gutter-cleaning') return GutterCleaningImage;
-	if (serviceSlug === 'patio-cleaning') return PatioCleaningImage;
-	if (serviceSlug === 'roof-cleaning') return RoofCleaningImage;
+	if (serviceSlug === 'blocked-drains') return BlockedDrains;
+	if (serviceSlug === 'cctv-drain-surveys') return CctvDrainSurveys;
+	if (serviceSlug === 'drain-repairs') return DrainRepairs;
+	if (serviceSlug === 'soakaways-septic-tanks') return Soakaways;
+	if (serviceSlug === 'gutter-cleaning') return GutterCleaning;
 
-	return HomeImage;
+	return Home;
 }
